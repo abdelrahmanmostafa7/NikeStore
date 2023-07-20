2fccd1526b626bd7b031961f4f06ed80f7486845
boyka <abdelrahmanzaki747@gmail.com>
2023-07-20 11:18:35 +0300

    sales & flexContent & slider sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Hero from "./components/Hero"
-import {heroapi} from "./data/data.js"
 import './index.css'
+import Hero from "./components/Hero"
+import { heroapi, popularsales, toprateslaes, highlight, sneaker , story} from "./data/data.js"
+import Sales from './components/Sales'
+import FlexContext from './components/FlexContext'
+import Stories from './components/Stories'
+import Footer from './components/Footer'
 function App() {
   return (
-    <main>
-      <Hero heroapi={heroapi}/>
+    <main className='flex flex-col gap-20 relative'>
+      <Hero heroapi={heroapi} />
+      <Sales endpoint={popularsales} ifExists />
+      <FlexContext endpoint={highlight} ifExists />
+      <Sales endpoint={toprateslaes} />
+      <FlexContext endpoint={sneaker} />
+      <Stories story={story}/>
+      <Footer />s
     </main>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer
-  ],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }]
-  }
+  plugins: [react()]
 })
